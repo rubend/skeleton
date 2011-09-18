6ffda56e2bea3de04aa3779d9cd4ad7ed9a937d5
Ruben Dezeure <ruben.dezeure@gmail.com>
2011-09-18 13:46:47 +0200

    wrote the main algorithm
    Still needs to be debugged and optimized.
    Important check -> can you index an vector with another vector or the
    likes?

--- graphfuncts.cpp
+++ graphfuncts.cpp
@@ -56,33 +56,34 @@ bool any(bool *G,int p)
 }
 
 /**
- * startrow = the row from which to start looking for a row with connections in the graph
+ * row = the row from which we want the connections in the graph
  * G = the graph
  * p = the number of rows
  * returns a integer vector containing the connections of the next 
  * row that still has connections.
+ * -1 signals end of connections
  */
-int[] getNextRowConnections(int startrow,bool* G,int p)
+int[] getNextRowConnections(int row,bool* G,int p)
 {
   int connections[p-1];//stores the connections we will return
-  connections[0] = -1; // if first element is -1 means we 
-  
-  int nextrow = getNextRowWithConnections(startrow,G,p);
   int index = 0;//keeping track of the index in the connections vector
   
-  if (nextrow != -1)
+  //there is still a row >= startrow with connections
+  for (int j = 0; j < p; ++j)
     {
-      //there is still a row >= startrow with connections
-      for (int j = i; j < p; ++j)
+      if (G[row*p+j] == true)
 	{
-	  if (G[row*p+j] == true)
-	    {
-	      connections[index]
-		}
+	  connections[index] = j;
+	  index++;  
 	}
-      return connections;
     }
-
+  
+  if (index != p-1)
+    {
+      //we didn't have p-1 connections
+      connections[index+1] = -1; // signal end
+    }
+  
   return connections;
 }
 
@@ -109,5 +110,41 @@ int getNextRowWithConnections(int startrow,bool* G,int p)
   
   //seems like there are no more rows >= startrows containing connections
   return -1;
+}
+
+/**
+ * get the other connection when excluding y
+ */
+std::vector getOtherConnections(int j,int[] connections)
+{
+  std::vector others(0);
+
+  for (int i = 0; i < p-1; ++i)
+    {
+      if (connections[i] == -1) break;
+
+      //we don't want y==connections[j] in here
+      if (i != j)
+	{
+	  others.push_back(connections[i]);
+	}
+    }
+  
+  return others;
+}
+
+/**
+ * Creates a vector of size ord with elements 1:ord
+ *
+ *
+ */
+std::vector getSeqVector(int ord)
+{
+  std::vector seq(ord);
+  for (int i = 0; i < ord; ++i)
+    {
+      seq[i] = i+1;
+    }
+  return seq;
 }
 
--- skeleton.cpp
+++ skeleton.cpp
@@ -35,9 +35,11 @@ using namespace Rcpp;
  * p number of variables
  * m_max max order to keep looking for
  */
-RcppExport SEXP main(SEXP n, SEXP p,SEXP alpha,SEXP m_max)
+RcppExport SEXP main(SEXP n, SEXP p,SEXP alpha,SEXP m_max, SEXP C)
 {
   // now this main will be implemented as the skeleton function! :p
+  
+  NumericMatrix Corr(C);
   
   //using c++ datatypes and trying to writing all functions myself, 
   //i.e. not calling R from c++
@@ -50,18 +52,62 @@ RcppExport SEXP main(SEXP n, SEXP p,SEXP alpha,SEXP m_max)
   int row = 0;// the current row we are studying the connections of
   int connections[p-1]; // stores the connections the current row has (maximum p-1)
   //static allocation ==> efficient
+  int sizeothers,x,y;
+  double pval;
   
   for (int ord = 0; ord <= m_max; ++ord)
     {
       //look for next row with connections then iterate over the remaining connections
       //alternative: save all connections explicitly in double array and loop over those.
       
+      row = getNextRowWithConnections(row,G,p); // row == x
+      x = row;
+      
+      while(row != -1)
+	{
+	  connections = getRowConnections(row,G,p); // getting the connections belonging to the row
+	  for (int i = 0; i < p-1; ++i)
+	    {
+	      //one of the remaining edge tests
+	      y=connections[i];
+	      
+	      //so now we are gonna check the correlation between row and connections[i]
+	      //in respect to every other subset of lengths ord of the remaining connections
+	      if(y == -1) break;//reached end of connections
+	      //y == connections[i]
+	      std::vector others = getOtherConnections(i,connections);
+	      sizeothers = others.size();
+	      if (sizeothers < ord)
+		{
+		  continue;//goto next loop iteration
+		}
+	      
+	      //initial subset, TODO is there a more efficient way? Builtin way for this?
+	      std::vector subset = getSeqVector(ord);
+	      
+	     
+	      while(subset[0] != -1)
+		{
+		  std::vector k = others[subset] //does this work? check! otherwise write own function that does this.
+		  //pval = pcorOrder(x,y,k,C)
+		  pval = pcorOrder(x,y,k,Corr);
+		  
+		  if (pval >= alpha)
+		    {
+		      //independent
+		      G[x,y]=false;G[y,x]=false;
+		      break; //no more checking to be done
+		    }
+		  
+		  subset = getNextSet(sizeothers,ord,subset);
+		}
+	      
+	    }
+	  row = getNextRowWithConnections(row+1,G,p);
+	}
     }
-
   
-  
-  
-  return wrap(G);
+  return wrap(G); // return graph matrix, in later stage return a more complete object
 }
 
 
